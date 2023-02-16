f6bb53c61293603512025344683d663469170b1d
hymmns <social.hymmns@gmail.com>
2023-02-17 00:32:33 +0100

    fix error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * Return: Always 1
  */
 int main(void)
-{
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+{{
+	eprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
