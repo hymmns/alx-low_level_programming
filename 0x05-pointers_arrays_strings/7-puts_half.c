4ae3bd7c53a0075698ebf066a340994f590d73ec
hymmns <social.hymmns@gmail.com>
2023-02-28 04:06:26 +0100

    prints half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, index = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		for (; index < (len / 2); index++) 
+			_putchar(str[index]);
+	else
+		for (index = (len - 1) / 2; index < len; index++)
+			_putchar(str[indx]);
+	_putchar('\n');
+}
+
