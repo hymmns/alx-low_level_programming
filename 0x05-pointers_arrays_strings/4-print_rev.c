881a46e36434367531497ede98585295e8f36342
hymmns <social.hymmns@gmail.com>
2023-02-28 02:55:13 +0100

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line
+ *@s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	for (; len >= 0; len--)
+		_putchar(*(s + len));
+	_putchar('\n');
+}
