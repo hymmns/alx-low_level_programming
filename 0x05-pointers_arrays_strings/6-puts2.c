de90e7c8e524566b5304b234e9e499badc4c9ce3
hymmns <social.hymmns@gmail.com>
2023-02-28 03:37:48 +0100

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string, starting with the first character, followed by a new line
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if ((index + 1) % 2 != 0)
+		       _putchar(*(str + index));
+		index++;
+	}
+	
+	_putchar('\n');
+}	
