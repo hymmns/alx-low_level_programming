19768dc5d889b3740ae998110457d55833746d21
hymmns <social.hymmns@gmail.com>
2023-02-28 17:00:42 +0100

    convert string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int ind = 0,marker = -1;
+	unsigned int res;
+
+	while( s[ind] != '\0')
+    	{
+        	unsigned int val = s[ind] - '0';
+
+        	if (!(val > 9) && val >= 0)
+            	{
+                	printf("%d", val);
+
+                	if (marker == -1 && ind > 0)
+                   		 marker = ind - 1;
+
+               		if(ind > 0)
+                  		res = (res * 10) + val;
+               		else
+                    		res = val;
+
+            	}
+        	ind++;
+    	}
+
+    	res = *(c + marker) == '-' ? -res : res;
+    	return (res);
+}
