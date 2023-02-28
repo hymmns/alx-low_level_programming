8a0fe42be1851939efade5cfc957a0b3eaa2b2fb
hymmns <social.hymmns@gmail.com>
2023-02-28 04:38:12 +0100

    copy till

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: destination
+ * @src: source
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while ( src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	
+	dest[index] = '\0';
+}
