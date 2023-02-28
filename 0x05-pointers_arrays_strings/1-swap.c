89d0bcf39cad94c07f055f343c5c8153d3ac0cce
hymmns <social.hymmns@gmail.com>
2023-02-28 02:18:22 +0100

    swap the actaul params

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of the actual parameters
+ * @a: first value 
+ * @b: second value
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
