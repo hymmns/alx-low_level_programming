2a2e9b829dc6bcdafcc9afb4a5da9cb16fb0d289
hymmns <social.hymmns@gmail.com>
2023-03-01 09:51:28 +0100

    fix betty style error

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,17 +7,18 @@
  */
 int _atoi(char *s)
 {
-	int ind = 0,marker = -1;
+	int ind = 0, marker = -1;
 	unsigned int res;
 
 	while (*(s + ind) != '\0')
 	{
 		int val = *(s + ind) - '0';
+
 		if (!(val > 9) && ((val == 0) || (val > 0)))
 		{
 			if (marker == -1 && ind > 0)
 				marker = ind - 1;
-			if(ind > 0)
+			if (ind > 0)
 				res = (res * 10) + val;
 			else
 				res = val;
