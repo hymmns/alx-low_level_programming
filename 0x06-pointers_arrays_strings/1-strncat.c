beeca8c37dac27e3a90a3d69f754e95255f72b2a
hymmns <social.hymmns@gmail.com>
2023-03-02 20:10:33 +0100

    fix issue

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,18 +10,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int destIndex = 0, srcIndex = 0;
+	int destIndex = 0, srcIndex = 0;
 
-        while (*(dest + destIndex) != '\0')
-                destIndex++;
-
-        for (; srcIndex < n; srcIndex++)
-        {
-                if (*(src + srcIndex) == '\0')
-                        break;
-
-                *(dest + destIndex) = *(src + srcIndex);
-                destIndex++;
-        }
-        return (dest);
+	while (des[destIndex])
+		destIndex++;
+	for (; srcIndex < n; srcIndex++)
+	{
+		*(dest + destIndex) = *(src + srcIndex);
+		if (src[srcIndex] == '\0')
+			break;
+		destIndex++;
+	}
+	return (dest);
 }
