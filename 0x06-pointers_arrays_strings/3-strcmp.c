3f2553b081a381addbc3946d7b57d1927af6f4f3
hymmns <social.hymmns@gmail.com>
2023-03-02 21:02:58 +0100

    fix error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
- * _strcmp - compares two strings
- * @s1: string
- * @s2: string
+ * _strcmp - compare two strings.
+ * @s1: first string.
+ * @s2: second string.
  *
- * Return: int
+ * Return: negative value if string is less than the other.
+ * positive value if string is greater than the other.
+ * 0 if strings are equal.
  */
-
 int _strcmp(char *s1, char *s2)
 {
-        int diff = 0, index = 0;
+	int index = 0, diff;
 
-        while (diff == 0 && *(s1 + index) != '\0' && *(s2 + index) != '\0')
-        {
-                diff = *(s1 + index) - *(s2 + index);
-                index++;
-        }
-        return (diff);
+	while (s1[index] == s2[index] && s1[index] != '\0')
+	{
+		index++;
+	}
+	diff = s1[index] - s2[index];
+	return (diff);
 }
