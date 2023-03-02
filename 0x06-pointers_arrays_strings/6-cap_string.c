4582bbcbc530a2394d629c3ccb001226c61f1a31
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-02 22:56:37 +0100

    fix issue

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,16 +11,16 @@ char *cap_string(char *s)
 	int index = 0, isLowerCase, isSpecialChar;
 	char prev;
 
-	while (*(s + index))
+	while (*(s + i))
 	{
-		isLower = *(s + index) >= 97 && *(s + index) <= 122;
+		isLowerCase = *(s + index) >= 97 && *(s + index) <= 122;
 		prev = index > 0 ? *(s + (index - 1)) : '\0';
-		isSpecialChar = prev == ' ' || prev == '.' || prev == '\n'
-			|| prev == '\t' || prev == ',' || prev == ';'
+		isSpecialChar = prev == ' ' || prev == '\t' || prev == '\n'
+			|| prev == ',' || prev == ';' || prev == '.'
 			|| prev == '!' || prev == '?' || prev == '"'
 			|| prev == '(' || prev == ')' || prev == '{'
 			|| prev == '}';
-		if (index == 0 && isLower)
+		if (index == 0 && isLowerCase)
 			*(s + index) = *(s + index) - 32;
 		else
 			if (isSpecialChar && isLowerCase)
