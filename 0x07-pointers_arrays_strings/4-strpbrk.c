d6acbe383f2edb22497ad68623bd2148097101b9
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-07 02:34:35 +0100

    searches a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string
+ *            for any of a set of bytes.
+ *
+ * @s: char pointer.
+ * @accept: char pointer.
+ *
+ * Return: char pointer to the bytes
+ *         or NULL if not found
+*/
+char *_strpbrk(char *s, char *accept)
+{
+        int sIndex = 0, aIndex;
+        while (s[sIndex] != '\0')
+        {
+                for (aIndex = 0; accept[aIndex] != '\0'; aIndex++)
+                {
+                        if (s[sIndex] == accept[aIndex])
+                                return (s + sIndex);
+                }
+                sIndex++;
+        }
+        return ('\0');
+}
