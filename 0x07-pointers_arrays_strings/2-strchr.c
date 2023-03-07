948724ec5ae454aa8d10c7adb5ef5fa2cbe59bd0
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-07 04:45:31 +0100

    fix bug

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
  * _strchr - locates a character in a string.
- *
  * @s: char pointer string.
  * @c: char character to locate.
  *
- * Return: char pointer first occurence of charatcer 
-   or null if not found
+ * Return: char pointer first occurence 
+ of charatcer or null if not found
 */
 char *_strchr(char *s, char c)
 {
