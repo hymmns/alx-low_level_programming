93e355e6e6f2d2e51333297b9dba4ff6363ff4e2
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-07 04:40:32 +0100

    fix issue

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * _memcpy - copies memory area.
+ * _memcpy - fill memory with a constant byte.
  * @dest: destination.
  * @src: source.
- * @n: n bytes from memory.
+ * @n: numbers of byte.
  *
- * Return: char pointer destination
-*/
+ * Return: char pointer.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-                
-        return (dest);
+
+	return (dest);
 }
