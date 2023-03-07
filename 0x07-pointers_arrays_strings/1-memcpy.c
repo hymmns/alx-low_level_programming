23792dd0be06dcd4317596e7ea7582e047dfc531
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-07 04:36:36 +0100

    fix bug

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
         unsigned int i;
 
-        i = 0;
-
-        for (; i < n; i++)
-                *(dest + i) = *(src + i);
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
                 
         return (dest);
 }
