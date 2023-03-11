28c048029db750f8afdff4aeeb2444d37b2116ad
hymmns <social.hymmns@gmail.com>
2023-03-11 02:04:59 +0100

    fix bug

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 /**
+ * main - starting point.
+ * @argc: int numbers of argument passed.
+ * @argv: pointer to arrays of passed arguments.
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	whatsmyname(argv[0]);
+	return (0);
+}
+
+/**
  * whatsmyname - prints the program name.
  * @s: string pointer.
  *
