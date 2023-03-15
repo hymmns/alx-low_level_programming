8be6ee56b427df0d997dba554c1f4cff207eb1f6
hymmns <social.hymmns@gmail.com>
2023-03-15 04:36:58 +0100

    fix bug

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,24 +11,38 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *strConcat = (char *) malloc(sizeof(char));
-	int s1Ind = 0, s2Ind = 0;
+	char *strConcat;
+	int s1Len = 0, s2Len = 0;
 
-	if (s1 == NULL || s2 == NULL || strConcat == NULL)
+	if (s1 != NULL)
+		while (*(s1 + s1Len) != '\0')
+			s1Len++;
+
+	if (s2 != NULL)
+		while (*(s2 + s2Len) != '\0')
+			s2Len++;
+
+	strConcat = (char *) malloc(sizeof(char) * (s1Len + s2Len));
+
+	if (strConcat == NULL)
 		return (NULL);
 
-	while (*(s1 + s1Ind) != '\0')
-	{
-		*(strConcat + s1Ind) = *(s1 + s1Ind);
-		s1Ind++;
-	}
+	s1Len = 0;
+	s2Len = 0;
 
-	while (*(s2 + s2Ind) != '\0')
-	{
-		*(strConcat + s1Ind) = *(s2 + s2Ind);
-		s2Ind++;
-		s1Ind++;
-	}
+	if (s1 != NULL)
+		while (*(s1 + s1Len) != '\0')
+		{
+			*(strConcat + s1Len) = *(s1 + s1Len);
+			s1Len++;
+		}
+	if (s2 != NULL)
+		while (*(s2 + s2Len) != '\0')
+		{
+			*(strConcat + s1Len) = *(s2 + s2Len);
+			s2Len++;
+			s1Len++;
+		}
 
 	return (strConcat);
 }
