69ef6f1cf34ea6a73a59d8bb9ecb3bfb5dc50af1
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-17 04:38:37 +0100

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: int - size of malloc.
+ *
+ * Return: Always void pointer.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
