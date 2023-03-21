653a3856176f549234cf210054cd67e909269399
hymmns <social.hymmns@gmail.com>
2023-03-21 20:09:37 +0100

    fix issues

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,11 +12,9 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	struct dog *p = d;
-
-	char *formatN = p->name == NULL ? "(nil)" : p->name;
-	float formatA = !p->age ? 0.0 : p->age;
-	char *formatO = p->owner == NULL ? "(nil)" : p->owner;
+	char *formatN = d->name == NULL ? "(nil)" : d->name;
+	float formatA = !d->age ? 0.0 : d->age;
+	char *formatO = d->owner == NULL ? "(nil)" : d->owner;
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", formatN, formatA, formatO);
 }
