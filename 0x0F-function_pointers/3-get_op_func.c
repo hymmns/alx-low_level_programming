7c545ad884da0bfa9eb417d4d6294cacea79e4d3
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-29 21:43:35 +0100

    Create 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - get ops function.
+ * @s: op type.
+ *
+ * Return: Always int.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"/", op_div},
+		{"*", op_mul},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (*(ops[i].op) == *s && strlen(s) == 1)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
