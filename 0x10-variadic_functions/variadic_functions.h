e603d8c0d034448784baec4ac783da76e5cc28c7
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-30 22:44:18 +0100

    add prototypes and struct

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,20 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_integer(const char *separator, va_list ap);
+void print_float(const char *separator, va_list ap);
+void print_char(const char *separator, va_list ap);
+void print_string(const char *separator, va_list ap);
+/**
+ * struct format_types - struct format_types
+ * @delimiter: char - delimiter.
+ * @callback: function pointer
+ */
+typedef struct format_types
+{
+	char delimiter;
+	void (*callback)(const char *separator, va_list ap);
+} format_types;
 
 #endif
