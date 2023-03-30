9887548573582630a202d40cf042c8d8177b42bb
hymmns <53025434+hymmns@users.noreply.github.com>
2023-03-30 18:41:04 +0100

    Create 0-putchar.c

---
+++ x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char *str = "_putchar";
+
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+	return (0);
+}
