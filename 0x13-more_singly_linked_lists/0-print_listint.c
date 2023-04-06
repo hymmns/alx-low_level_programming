5068eba0ad0c5d92cc1127cd61d23b6156aa7d23
hymmns <social.hymmns@gmail.com>
2023-04-06 01:56:21 +0100

    fix bug

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num_of_nodes = 0;
+	const listint_t current_node = h;
 
-	while (h != NULL)
+	while (current_node != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
+		current_node = h->next;
 		num_of_nodes++;
 	}
 
