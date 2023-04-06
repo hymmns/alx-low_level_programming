565758402f025b876e3d54abe44074eb65d94a4f
hymmns <social.hymmns@gmail.com>
2023-04-06 02:01:26 +0100

    fix error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,12 +12,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num_of_nodes = 0;
-	const listint_t current_node = h;
+	const listint_t *current_node = h;
 
 	while (current_node != NULL)
 	{
-		printf("%d\n", h->n);
-		current_node = h->next;
+		printf("%d\n", current_node->n);
+		current_node = current_node->next;
 		num_of_nodes++;
 	}
 
