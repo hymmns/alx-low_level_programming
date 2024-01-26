4dbe33d76b2b97f68e48876ccb3abb60366dd68d
hymmns <esholadoye@gmail.com>
2024-01-26 17:17:40 +0100

    fix requirement fail

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stddef.h>
-#include <stdbool.h>
 #include "lists.h"
 
 /**
@@ -13,17 +12,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num_of_nodes = 0;
 
-	while (true)
+	while (h != NULL)
 	{
-		if (h == NULL)
-			break;
-
-		else
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			num_of_nodes++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		num_of_nodes++;
 	}
 
 	return (num_of_nodes);
