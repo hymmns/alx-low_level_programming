0f36766e1825ace0a4b0a15a005e44c711d97e82
hymmns <esholadoye@gmail.com>
2024-01-28 19:26:41 +0100

    fix c99 or c11 mode error

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,9 +6,11 @@
 
 int main(void)
 {
-	for (int digit_one = 0; digit_one < 10; digit_one++)
+	int digit_one, digit_two;
+
+	for (digit_one = 0; digit_one < 10; digit_one++)
 	{
-		for (int digit_two = digit_one + 1; digit_two < 10; digit_two++)
+		for (digit_two = digit_one + 1; digit_two < 10; digit_two++)
 		{
 			putchar(digit_one + '0');
 			putchar(digit_two + '0');
