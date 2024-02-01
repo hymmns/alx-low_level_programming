2b5c807b9820b0d8b411a080a8a227d82b3943a3
hymmns <esholadoye@gmail.com>
2024-02-01 02:13:17 +0100

    prints the n times table, starting with 0.

--- 0x01-variables_if_else_while/100-times_table.c
+++ 0x01-variables_if_else_while/100-times_table.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with
- * 0
+ * print_times_table - prints the n times table, starting with 0
  * @n: int
  *
  * Return: void
  */
 void print_times_table(int n)
 {
-
 	if (n > 15 || n < 0)
 		return;
 
@@ -21,22 +19,33 @@ void print_times_table(int n)
 		{
 			int result = multiplier * multiplicand;
 
-			printf("%d", result);
-
-			if (multiplicand < n)
+			if (multiplicand > 0)
 			{
-				printf(",");
-
-				if (result > 99)
-					printf(" ");
-
-				else if (result > 9)
-					printf("  ");
-				else
-					printf("   ");
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (result < 99)
+				{
+					_putchar(' ');
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+				else if (result < 999)
+				{
+					_putchar(result / 100 + '0');
+					_putchar((result % 100) / 10 + '0');
+					_putchar((result % 100) % 10 + '0');
+				}
 			}
+			if (result < 10)
+				_putchar(result + '0');
+
 		}
-		printf("\n");
+		_putchar('\n');
 	}
-	printf("\n");
+	_putchar('\n');
 }
